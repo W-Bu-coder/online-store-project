65c0b38f92740aa3e57da5af1419d25d5bfe751a
Wenxiao <buwenxiao@outlook.com>
2024-11-11 10:24:38 -0500

    clarify the format of front-end interaction

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,24 @@ app.use(cors({
   origin: 'http://localhost:3000' // frontend url
 }));
 
-app.get('/hello', (req, res) => {
-  console.log('Received: ')
-  console.log(req)
-  let data = JSON.stringify('Hello World!')
-  res.send(data)
-})
-
 app.listen(port, () => {
   console.log(`Backend is listening on port ${port}`)
 })
+
+const createResponse = (data, code = 200, message = "success") => {
+  return {
+    code,
+    message,
+    data
+  }
+}
+
+app.get('/hello', (req, res) => {
+  console.log('Received: ')
+  console.log(req)
+  let data = {
+    content: 'Hello, World!'
+  }
+  res.send(createResponse(data))
+})
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ const api = "http://localhost:3036"
 
 const handleClick = () => {
   let url = api + '/hello';
-  console.log('Sending request to:', url); // 打印请求 URL
+  console.log('Sending request to:', url);
 
   fetch(url, {
     method: 'GET',
@@ -12,15 +12,10 @@ const handleClick = () => {
       'Content-Type': 'application/json'
     },
   })
-    .then(res => {
-      console.log('Response status:', res.status); // 打印响应状态
-      return res.json();
-    })
+    .then(res => res.json())
     .then(data => console.log('Response data:', data))
     .catch(error => {
-      console.error('Fetch error:', error); // 详细错误信息
-      console.error('Error name:', error.name);
-      console.error('Error message:', error.message);
+      console.error('Fetch error:', error);
     });
 };
 
